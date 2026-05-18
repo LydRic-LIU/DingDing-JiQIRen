0bee8296387725d298e7cb2edb6e29ab88aee3ac
Lydric <106065800+LydRic-LIU@users.noreply.github.com>
2026-05-18 17:06:40 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+DINGTALK_WEBHOOK = "https://oapi.dingtalk.com/robot/send?access_token=7d404cdd91f27a4c4a839e51d921a37f00f0ba25c6126056075f77b2e4fb66b7"
+
+PUSH_KEYWORD = "选品通知"
+
+TARGET_REGIONS = [
+    "US", "CA", "GB", "DE", "FR", "ES", "IT"
+]
+
+CATEGORIES = [
+    "Beauty",
+    "Home",
+    "Tech"
+]
+
+SCHEDULE_TIMES = [
+    "09:00",
+    "13:00",
+    "17:00"
+]

5eafc18b39d3515d4480dd751b9c25683495ac54
Lydric <106065800+LydRic-LIU@users.noreply.github.com>
2026-05-18 17:30:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-DINGTALK_WEBHOOK = "https://oapi.dingtalk.com/robot/send?access_token=7d404cdd91f27a4c4a839e51d921a37f00f0ba25c6126056075f77b2e4fb66b7"
+import os
+
+DINGTALK_WEBHOOK = os.getenv("DINGTALK_WEBHOOK")
 
 PUSH_KEYWORD = "选品通知"
 
@@ -10,10 +12,4 @@ CATEGORIES = [
     "Beauty",
     "Home",
     "Tech"
-]
-
-SCHEDULE_TIMES = [
-    "09:00",
-    "13:00",
-    "17:00"
 ]
